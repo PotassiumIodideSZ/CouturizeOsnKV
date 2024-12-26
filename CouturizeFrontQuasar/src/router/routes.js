4b58f173c2bed085fa8b9e9ce27190129a35dc9c
PotassiumIodideSZ <skobelkin.zahar@gmail.com>
2024-12-26 17:26:45 +0500

    uncommented

--- CouturizeFrontQuasar/src/router/routes.js
+++ CouturizeFrontQuasar/src/router/routes.js
@@ -5,8 +5,8 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
       // { path: 'main', component: () => import('pages/MainPage.vue') },
-      // { path: 'login', component: () => import('pages/LoginPage.vue') },
-      // { path: 'register', component: () => import('pages/RegisterPage.vue') },
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+      { path: 'register', component: () => import('pages/RegisterPage.vue') },
       // { path: 'style', component: () => import('pages/StylePage.vue') },
       // { path: '/profile', name: 'Profile', component: () => import('pages/ProfilePage.vue') },
     ]
